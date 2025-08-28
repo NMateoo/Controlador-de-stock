82b7b04a541d5cd4458a123f0f39c4d70b4e5cf6
Nicolás Mateo Macías <55945912+NMateoo@users.noreply.github.com>
2025-08-28 15:31:16 +0200

    Para Vercel

--- camisetas-stock/api/index.ts
+++ camisetas-stock/api/index.ts
@@ -1,3 +1,5 @@
-import { app } from '../../server';
+// Importa el servidor ya compilado por Angular SSR
+// Esto asegura que las rutas y paths internos (browserDistFolder, indexHtml) sean correctos en Vercel
+import app from '../dist/camisetas-stock/server/server.mjs';
 
-export default app; 
+export default app;
